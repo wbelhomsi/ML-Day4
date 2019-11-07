bdcc1708d30a341ea6583f7f93dbec8d4aba06c1
INCIBMB <50316838+INCIBMB@users.noreply.github.com>
2019-11-07 10:20:35 +0200

    Add files via upload

---
+++ Day4_RNN.py
@@ -0,0 +1,6 @@
+from __future__ import print_function
+from keras.layers import Dense, Activation
+from keras.layers.recurrent import SimpleRNN
+from keras.models import Sequential
+import numpy as np
+
